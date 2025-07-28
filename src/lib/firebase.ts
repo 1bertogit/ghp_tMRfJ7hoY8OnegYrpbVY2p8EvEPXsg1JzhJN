1602b00e5b373acbb7e3e3170c31f14294db6e40
Humberto Lopes <humbertobrunolopes@gmail.com>
2025-07-28 00:09:35 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: "visioncare-mentorship",
+  appId: "1:403399724145:web:1f62b0560ab676158a1fd0",
+  storageBucket: "visioncare-mentorship.firebasestorage.app",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "visioncare-mentorship.firebaseapp.com",
+  messagingSenderId: "403399724145",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
